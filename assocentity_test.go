0d938f2f76666b2cc9abf32b0c12686c5f94309a
Julian Claus <kontakt@julian-claus.de>
2019-03-10 10:04:57 +0100

    test: use table test, closes #1

--- assocentity_test.go
+++ assocentity_test.go
@@ -5,114 +5,94 @@ import (
 	"testing"
 )
 
-func TestRomance_Empty(t *testing.T) {
-	text := "Hello world"
-	entity := "Bye"
+func TestRomance_SingleWord(t *testing.T) {
+	tests := []struct {
+		name   string
+		text   string
+		entity string
+		res    map[string]float64
+	}{
+		{"empty", "Hello world", "Bye", map[string]float64{}},
+		{"subset", "Hello world", "Helloworld", map[string]float64{}},
+		{"subset", "Hello world", "Helloworld", map[string]float64{}},
+		{"simple", "Hello, my name is Max Payne.", "Max", map[string]float64{
+			"Hello": 4,
+			"my":    3,
+			"name":  2,
+			"is":    1,
+			"Payne": 1,
+		}},
+	}
 
-	res := Romance(text, entity)
-	if !reflect.DeepEqual(res, map[string]float64{}) {
-		t.Errorf("TestRomance_Empty: Not equal")
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := Romance(test.text, test.entity)
+			if !reflect.DeepEqual(actual, test.res) {
+				t.Errorf("Romance(%v): expected %v, actual %v,", test.name, test.res, actual)
+			}
+		})
 	}
 }
 
-func TestRomance_Subset(t *testing.T) {
-	text := "Hello world"
-	entity := "Helloworld"
-
-	res := Romance(text, entity)
-	if !reflect.DeepEqual(res, map[string]float64{}) {
-		t.Errorf("TestRomance_Empty: Not equal")
-	}
-}
-
-func TestRomance_Start(t *testing.T) {
-	text := "Shang Tsung is my name."
-	entity := "Shang Tsung"
-
-	res := Romance(text, entity)
-	m := map[string]float64{
-		"is":   1,
-		"my":   2,
-		"name": 3,
-	}
-	if !reflect.DeepEqual(res, m) {
-		t.Errorf("TestRomance_Empty: Not equal")
-	}
-}
-
-func TestRomance_simpleOneWord(t *testing.T) {
-	text := "Hello, my name is Max Payne."
-	entity := "Max"
-
-	res := Romance(text, entity)
-	m := map[string]float64{
-		"Hello": 4,
-		"my":    3,
-		"name":  2,
-		"is":    1,
-		"Payne": 1,
-	}
-
-	if !reflect.DeepEqual(res, m) {
-		t.Errorf("TestRomance_simpleOneWord: Not equal")
-	}
-}
-func TestRomance_simpleTwoWords(t *testing.T) {
-	text := "Hello, my name is Max Payne."
-	entity := "Max Payne"
-
-	res := Romance(text, entity)
-	m := map[string]float64{
-		"Hello": 4,
-		"my":    3,
-		"name":  2,
-		"is":    1,
-	}
-
-	if !reflect.DeepEqual(res, m) {
-		t.Errorf("TestRomance_simpleTwoWords: Not equal")
-	}
-}
-
-func TestRomance_simpleFourWords(t *testing.T) {
-	text := `If you smell, what Dwayne "The Rock" Johnson is cooking?`
-	entity := `Dwayne "The Rock" Johnson`
-
-	res := Romance(text, entity)
-	m := map[string]float64{
-		"If":      4,
-		"you":     3,
-		"smell":   2,
-		"what":    1,
-		"is":      1,
-		"cooking": 2,
+func TestRomance_MultiWord(t *testing.T) {
+	tests := []struct {
+		name   string
+		text   string
+		entity string
+		res    map[string]float64
+	}{
+		{"empty", "Hello world", "Bye", map[string]float64{}},
+		{"subset", "Hello world", "Helloworld", map[string]float64{}},
+		{"start", "Shang Tsung is my name.", "Shang Tsung", map[string]float64{
+			"is":   1,
+			"my":   2,
+			"name": 3,
+		}},
+		{"simple", "Hello, my name is Max Payne.", "Max Payne", map[string]float64{
+			"Hello": 4,
+			"my":    3,
+			"name":  2,
+			"is":    1,
+		}},
+		{
+			"inline",
+			`If you smell, what Dwayne "The Rock" Johnson is cooking?`,
+			`Dwayne "The Rock" Johnson`,
+			map[string]float64{
+				"If":      4,
+				"you":     3,
+				"smell":   2,
+				"what":    1,
+				"is":      1,
+				"cooking": 2,
+			},
+		},
+		{
+			"inline multi",
+			`Shao Kahn is the embodiment of evil. Shao Kahn is easily recognizable by his intimidating stature.`,
+			"Shao Kahn",
+			map[string]float64{
+				"is":           3.75,
+				"the":          3,
+				"embodiment":   3,
+				"of":           3,
+				"evil":         3,
+				"easily":       5.5,
+				"recognizable": 6.5,
+				"by":           7.5,
+				"his":          8.5,
+				"intimidating": 9.5,
+				"stature":      10.5,
+			},
+		},
 	}
 
-	if !reflect.DeepEqual(res, m) {
-		t.Errorf("TestRomance_simpleTwoWords: Not equal")
-	}
-}
-
-func TestRomance_complexTwoWords(t *testing.T) {
-	text := "Shao Kahn is the embodiment of evil. Shao Kahn is easily recognizable by his intimidating stature."
-	entity := "Shao Kahn"
-
-	res := Romance(text, entity)
-	m := map[string]float64{
-		"is":           3.75,
-		"the":          3,
-		"embodiment":   3,
-		"of":           3,
-		"evil":         3,
-		"easily":       5.5,
-		"recognizable": 6.5,
-		"by":           7.5,
-		"his":          8.5,
-		"intimidating": 9.5,
-		"stature":      10.5,
-	}
-
-	if !reflect.DeepEqual(res, m) {
-		t.Errorf("TestRomance_complexTwoWords: Not equal")
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			actual := Romance(test.text, test.entity)
+			if !reflect.DeepEqual(actual, test.res) {
+				t.Errorf("Romance(%v): expected %v, actual %v,", test.name, test.res, actual)
+			}
+		})
 	}
 }
