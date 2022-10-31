9e5e6497a87c132169ebfc86a360dbee775a69ac
Julian Claus <kontakt@julian-claus.de>
2022-10-31 16:00:04 +0100

    feat: working version

--- cmd/bin.go
+++ cmd/bin.go
@@ -1,24 +1,28 @@
 package main
 
 import (
+	"context"
+	"encoding/csv"
 	"errors"
 	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
 	"strings"
-	"time"
 
 	"github.com/ndabAP/assocentity/v9"
 	"github.com/ndabAP/assocentity/v9/nlp"
 	"github.com/ndabAP/assocentity/v9/tokenize"
-	"golang.org/x/net/context"
 )
 
 var logger = log.Default()
 
 func init() {
 	log.SetFlags(0)
+	log.SetPrefix("assocentitiy: ")
+
+	logger.SetOutput(os.Stderr)
 }
 
 var poSMap = map[string]tokenize.PoS{
@@ -38,76 +42,83 @@ var poSMap = map[string]tokenize.PoS{
 	"x":       tokenize.X,
 }
 
-const default_timeout = 60
+var (
+	errMissingText     = errors.New("missing text")
+	errMissingEntities = errors.New("missing entities")
 
-func main() {
-	// Flags
-	_ = flag.Int(
-		"timeout",
-		60,
-		"Max allowed timeout per request",
-	)
-	gogSvcLocF := flag.String(
+	gogSvcLocF = flag.String(
 		"gog-svc-loc",
 		"",
 		"Google Clouds NLP JSON service account file, example: -gog-svc-loc=\"~/gog-svc-loc.json\"",
 	)
-	posF := flag.String(
+	posF = flag.String(
 		"pos",
 		"any",
 		"Defines part of speeches to keep, example: -pos=noun,verb,pron",
 	)
-	entitiesF := flag.String(
+	entitiesF = flag.String(
 		"entities",
 		"",
-		"Define entities to be searched within input, example: -entities=\"Donald Trump, Trump\"",
+		"Define entities to be searched within input, example: -entities=\"Max Payne, Payne\"",
 	)
+)
+
+func main() {
+	ctx := context.TODO()
+
 	flag.Parse()
 
 	textBytes, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
-		printUsageAndExit(err)
-	}
-	if len(textBytes) == 0 {
-		printUsageAndExit(errors.New("missing text"))
+		printUsageAndFail(err)
 	}
 
 	// assocentity
-	// TODO!: Fix timeout
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*default_timeout)
-	defer cancel()
-
 	// Create a NLP instance
 	credentialsFilename := *gogSvcLocF
 	nlpTok := nlp.NewNLPTokenizer(credentialsFilename, nlp.AutoLang)
 
-	// Allow any part of speech
+	// Set part of speech
 	posArr := strings.Split(*posF, ",")
 	pos := parsePoS(posArr)
 	posDeterm := nlp.NewNLPPoSDetermer(pos)
 
+	// Prepare text and entities
 	text := string(textBytes)
 	entities := strings.Split(*entitiesF, ",")
+
 	assocEntities, err := assocentity.Do(ctx, nlpTok, posDeterm, text, entities)
 	if err != nil {
-		printUsageAndExit(err)
+		printUsageAndFail(err)
 	}
 
-	logger.Println(assocEntities)
+	printAssocEntities(assocEntities)
+}
+
+func printAssocEntities(assocEntities map[string]float64) {
+	w := csv.NewWriter(os.Stdout)
+	defer w.Flush()
+
+	for token, dist := range assocEntities {
+		record := []string{
+			token, fmt.Sprintf("%v", dist),
+		}
+		w.Write(record)
+	}
 }
 
 func parsePoS(posArr []string) (pos tokenize.PoS) {
 	for _, p := range posArr {
 		if p, ok := poSMap[p]; ok {
-			pos &= p
+			// Add bits
+			pos += p
 		}
 	}
 	return
 }
 
-func printUsageAndExit(err error) {
-	logger.SetOutput(os.Stderr)
-	logger.Println("assocentitiy:", err)
+func printUsageAndFail(err error) {
+	logger.Println(err)
 	logger.Println()
 
 	logger.Println("Usage:")
